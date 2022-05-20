514bbc800233286608940e027ec3dc4cf781dd07
smhrd <smhrd@59.0.249.7>
2022-05-20 16:17:01 +0900

    유저 충돌 23

--- project2_h5/src/main/java/com/smhrd/domain/User.java
+++ project2_h5/src/main/java/com/smhrd/domain/User.java
@@ -4,105 +4,105 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 public class User {
-   public String user_id;
-   public String user_pw;
-   public String user_nick;
-   public String user_addr;
-   public BigDecimal user_manner;
-   public Timestamp user_joinDate;
-   
-   public String a; //임의의 값 선언
+	public String user_id;
+	public String user_pw;
+	public String user_nick;
+	public String user_addr;
+	public BigDecimal user_manner;
+	public Timestamp user_joinDate;
+	
+	public String a; //임의의 값 선언
 
-   
-   
+	
+	
 
-   public User(String user_id, String user_pw, String user_nick, String user_addr, BigDecimal user_manner,
-         Timestamp user_joinDate) {
-      super();
-      this.user_id = user_id;
-      this.user_pw = user_pw;
-      this.user_nick = user_nick;
-      this.user_addr = user_addr;
-      this.user_manner = user_manner;
-      this.user_joinDate = user_joinDate;
-   }
-
-
-   public User(String user_id, String user_pw) {
-      super();
-      this.user_id = user_id;
-      this.user_pw = user_pw;
-   }
+	public User(String user_id, String user_pw, String user_nick, String user_addr, BigDecimal user_manner,
+			Timestamp user_joinDate) {
+		super();
+		this.user_id = user_id;
+		this.user_pw = user_pw;
+		this.user_nick = user_nick;
+		this.user_addr = user_addr;
+		this.user_manner = user_manner;
+		this.user_joinDate = user_joinDate;
+	}
 
 
-
-   public User(String user_nick, String user_addr, String a) {
-      super();
-      this.user_nick = user_nick;
-      this.user_addr = user_addr;
-      //this.a = a; 얘도 꼭넣어줘야하나?? 
-      
-   }
-
-
-   public User(String user_id, String user_pw, String user_nick, String user_addr) {
-      super();
-      this.user_id = user_id;
-      this.user_pw = user_pw;
-      this.user_nick = user_nick;
-      this.user_addr = user_addr;
-   }
+	public User(String user_id, String user_pw) {
+		super();
+		this.user_id = user_id;
+		this.user_pw = user_pw;
+	}
 
 
 
+	public User(String user_nick, String user_addr, String a) {
+		super();
+		this.user_nick = user_nick;
+		this.user_addr = user_addr;
+		//this.a = a; 얘도 꼭넣어줘야하나?? 
+		
+	}
 
 
-   public String getUser_id() {
-      return user_id;
-   }
+	public User(String user_id, String user_pw, String user_nick, String user_addr) {
+		super();
+		this.user_id = user_id;
+		this.user_pw = user_pw;
+		this.user_nick = user_nick;
+		this.user_addr = user_addr;
+	}
 
 
 
 
 
-   public String getUser_pw() {
-      return user_pw;
-   }
+	public String getUser_id() {
+		return user_id;
+	}
 
 
 
 
 
-   public String getUser_nick() {
-      return user_nick;
-   }
+	public String getUser_pw() {
+		return user_pw;
+	}
 
 
 
 
 
-   public String getUser_addr() {
-      return user_addr;
-   }
+	public String getUser_nick() {
+		return user_nick;
+	}
 
 
 
 
 
-   public BigDecimal getUser_manner() {
-      return user_manner;
-   }
+	public String getUser_addr() {
+		return user_addr;
+	}
 
 
 
 
 
-   public Timestamp getUser_joinDate() {
-      return user_joinDate;
-   }
+	public BigDecimal getUser_manner() {
+		return user_manner;
+	}
 
 
 
 
-   
+
+	public Timestamp getUser_joinDate() {
+		return user_joinDate;
+	}
+
+
+
+
+	
 }
