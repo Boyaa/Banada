879b10b9769a02a27e639e8483a0a436967eacc2
smhrd <smhrd@61.80.80.189>
2022-05-20 16:38:34 +0900

    푸쉬하려고 풀

--- project2_h5/src/main/java/com/smhrd/controller/HobbyCon.java
+++ project2_h5/src/main/java/com/smhrd/controller/HobbyCon.java
@@ -6,17 +6,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HobbyCon
- */
+import com.smhrd.domain.Hobby;
+import com.smhrd.domain.HobbyDAO;
+
+
 public class HobbyCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+	  	
+		 
+	    String h_title = request.getParameter("h_title");
+		String h_content = request.getParameter("h_content");
+		String h_cate = request.getParameter("h_cate");
+		int h_maxpeople = Integer.parseInt(request.getParameter("h_maxpeople"));
+		
+		
+		HobbyDAO dao = new HobbyDAO();
+		// hobby 객체에 담기 - 글 쓸 때
+		Hobby hobby = new Hobby(h_title, h_content, h_cate, h_maxpeople);
+		// selectHobby - 글 읽을 때
+		
+		int cnt = dao.insertHobby(hobby);
+		
+		if(cnt>0) {
+			System.out.println("취미 카테고리 글 등록 성공");
+		} else {
+			System.out.println("취미 카테고리 글 등록 실패");
+		}
 	}
 
 }
